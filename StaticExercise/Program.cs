f7ed8780ad9ff4daee6866ffaa5a4141dc12fd5e
eopong87 <eopong87@gmail.com>
2023-07-05 15:32:16 -0400

    degrees coverter

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -5,6 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            double fahrenheit = 77;
+            double celsius = TempConverter.FahrenheitToCelsius(fahrenheit);
+
+
+            double celsius1 = 15;
+            double fahrenheit1 = TempConverter.CelsiusToFahrenheit(celsius1);
+
+            Console.WriteLine(celsius);
+            Console.WriteLine(fahrenheit1);
         }
+          
+
+        
     }
 }
